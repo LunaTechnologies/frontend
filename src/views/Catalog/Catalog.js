00b231fc5e07d7b50a7d42955c81b4483932a64a
Amar Kayed <amar.kayed18@gmail.com>
2022-05-06 15:05:09 +0300

    Started Catalog View
    This commit contains only the files and folders for the Catalog page.

---
+++ src/views/Catalog/Catalog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const Catalog = () => {
+  return (
+    <SafeAreaView>
+      <Text>Catalog</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Catalog;
