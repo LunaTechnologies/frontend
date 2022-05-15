fd9b0224a6d09271c3e3a51796ee3a03a07ad4bd
Amar Kayed <amar.kayed18@gmail.com>
2022-05-15 19:36:03 +0300

    Fixed TextInput Keyboard Dismiss
    Fixed the issue where the keyboard doesn't get dismissed once we tap outside the TextInput box.

--- src/components/PostFieldTextInput/PostFieldTextInput.js
+++ src/components/PostFieldTextInput/PostFieldTextInput.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, TextInput, Dimensions} from 'react-native';
+import {
+  SafeAreaView,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Dimensions,
+} from 'react-native';
 import PostFieldTextInputStyles from './PostFieldTextInputStyles';
 
 const {width, height} = Dimensions.get('window');
@@ -11,33 +17,38 @@ const PostFieldTextInput = props => {
   //   }, [value]);
   return (
     <SafeAreaView>
-      <TextInput
-        style={{
-          ...PostFieldTextInputStyles.textInput,
-          // width: parseFloat(props.width) * width,
-          // width: parseFloat(props.width),
-          width:
-            'width' in props
-              ? parseFloat(props.width) * width
-              : PostFieldTextInputStyles.textInput.width,
-          height:
-            'height' in props
-              ? parseFloat(props.height) * height
-              : PostFieldTextInputStyles.textInput.height,
-          textAlignVertical:
-            'textAlignVertical' in props ? props.textAlignVertical : 'center',
-        }}
-        multiline={
-          !('multiline' in props) || props.multiline == false ? false : true
-        }
-        value={value}
-        onChangeText={setValue}
-        keyboardType={
-          !('keyboardType' in props) || props.keyboardType != 'numeric'
-            ? 'default'
-            : 'numeric'
-        }
-        placeholder={props.placeholder}></TextInput>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}>
+        <TextInput
+          style={{
+            ...PostFieldTextInputStyles.textInput,
+            // width: parseFloat(props.width) * width,
+            // width: parseFloat(props.width),
+            width:
+              'width' in props
+                ? parseFloat(props.width) * width
+                : PostFieldTextInputStyles.textInput.width,
+            height:
+              'height' in props
+                ? parseFloat(props.height) * height
+                : PostFieldTextInputStyles.textInput.height,
+            textAlignVertical:
+              'textAlignVertical' in props ? props.textAlignVertical : 'center',
+          }}
+          multiline={
+            !('multiline' in props) || props.multiline == false ? false : true
+          }
+          value={value}
+          onChangeText={setValue}
+          keyboardType={
+            !('keyboardType' in props) || props.keyboardType != 'numeric'
+              ? 'default'
+              : 'numeric'
+          }
+          placeholder={props.placeholder}></TextInput>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
