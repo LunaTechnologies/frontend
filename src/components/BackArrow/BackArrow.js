084bf95e3979c9102e5f55d9fa80a2e7ddd37c91
mstanciu552 <mstanciu552@gmail.com>
2022-05-27 09:29:22 +0300

    (feat) added fonts

--- src/components/BackArrow/BackArrow.js
+++ src/components/BackArrow/BackArrow.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/AntDesign';
 import BackArrowStyles from './BackArrowStyles';
 
-const BackArrow = ({ color }) => {
+const BackArrow = ({ color, handler }) => {
   return (
-    <SafeAreaView style={BackArrowStyles.global}>
+    <TouchableOpacity style={BackArrowStyles.global} onPress={handler}>
       <Icon name="arrowleft" style={{ ...BackArrowStyles.arrow, color }} />
-    </SafeAreaView>
+    </TouchableOpacity>
   );
 };
 
